c6da27d754671cad5d5175130120c4d08c778fbc
cheetah <azaksk29@nate.com>
2020-02-02 17:19:51 +0900

    Use Switch, Redirect of react-router-dom to handle wrong URL

--- src/MyRouter.js
+++ src/MyRouter.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { 
+	BrowserRouter as Router, 
+	Switch,
+	Redirect,
+	Route
+} from "react-router-dom";
 import Home from './Router/Home'
 import Movie from './Router/Movie'
 import TVshow from './Router/TV'
@@ -7,12 +12,13 @@ import Search from './Router/Search'
 
 export default () => (
 	<Router>
-		<>
+		<Switch>
 			<Route path="/" exact component={Home} />
 			<Route path="/movie" component={Movie} />
 			<Route path="/tv" component={TVshow} />
 			<Route path="/search" component={Search} />
-		</>
+			<Redirect from="*" to="/" / >
+		</Switch>
 	</Router>
 )
 
